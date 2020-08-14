9b3fe7506c907cb62082dc50b41e1e1637c08ff9
butterfly1of4 <jhouck29@gmail.com>
2020-08-14 16:28:39 -0400

    cleaned up

--- lib/main.py
+++ lib/main.py
@@ -1,12 +1,12 @@
 from peewee import *
 from models import Contacts
 
-    
+
 print("My Contacts:")
 
 on = True
-while True:   
-        
+while True:
+
     def intro():
         print("Please choose from the following menu options: \n")
         print("1: For a list of All Contacts: Type 'all'; \n")
@@ -18,45 +18,40 @@ while True:
     def findAll():
         all = Contacts.select()
         for entry in all:
-            print(entry.first_name, entry.last_name,entry.number,entry.email)
-    # findAll()
-    if menu == 'all': #and on == True:       
+            print(entry.first_name, entry.last_name, entry.number, entry.email)
+
+    if menu == 'all':
         findAll()
-        # restart()
+
         continue
-        # break
-    # on==False
-    # intro()
-#CREATE 
+
+# CREATE
     def createContact():
-    # on == True
+
         first = input("First Name: ")
         last = input("Last Name: ")
         number = input("Phone Number: ")
         email = input("Email: ")
-        Contacts.create(first_name = first, last_name =last, number = number, email=email)
+        Contacts.create(first_name=first, last_name=last,
+                        number=number, email=email)
 
     if (menu == 'new'):
         createContact()
-        # restart()
+
         continue
 
-#FIND ONE     
+# FIND ONE
     def findName():
-    # on ==True
+
         person = Contacts.get(Contacts.first_name == menu)
         print(person.first_name, person.last_name, person.number, person.email)
-    # findName()
     if menu == Contacts.first_name:
         findName()
-    # on == False
-        # restart()  
         continue
 
     def exit():
         if menu == 'exit':
-            on  == False
+            on == False
             print("Goodbye")
     exit()
     break
-    
